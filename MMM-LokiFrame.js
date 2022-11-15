6543ee71ab9db265cc4f75f3aa1cfd67d63b0cb8
Noel Gomez <nollgmz4@gmail.com>
2022-11-15 09:55:19 -0800

    serves photos

--- MMM-LokiFrame.js
+++ MMM-LokiFrame.js
@@ -1,13 +1,48 @@
 Module.register("MMM-LokiFrame", {
-  // Default module config.
-  defaults: {
-    text: "Hello World!",
+  nextPhotoPath: null,
+  prevPhotoPath: null,
+  start: function () {
+    this.sendSocketNotification("GET_FIRST_IMAGES");
   },
-
-  // Override dom generator.
+  createImg: function (path) {
+    let imgWrapper = document.createElement("div");
+    let img = document.createElement("img");
+    imgWrapper.classList.add("image");
+    let backgroundImg = document.createElement("img");
+    backgroundImg.classList.add("background");
+    if (path) {
+      img.src = `/photos/${path}`;
+      backgroundImg.src = `/photos/${path}`;
+    }
+    imgWrapper.appendChild(backgroundImg);
+    imgWrapper.appendChild(img);
+    return imgWrapper;
+  },
   getDom: function () {
-    var wrapper = document.createElement("div");
-    wrapper.innerHTML = this.config.text;
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("fullscreen");
+    let topImg = this.createImg(this.prevPhotoPath);
+    let btmImg = this.createImg(this.nextPhotoPath);
+    if (this.nextPhotoPath) {
+      topImg.classList.add("fadeOut");
+    }
+    wrapper.appendChild(btmImg);
+    wrapper.appendChild(topImg);
     return wrapper;
+  },
+  getStyles() {
+    return ["lokiFrame.css"];
+  },
+  socketNotificationReceived: function (notification, payload) {
+    switch (notification) {
+      case "GET_FIRST_IMAGES":
+      case "GET_NEXT_IMAGE":
+        this.nextPhotoPath = payload.nextPath;
+        this.prevPhotoPath = payload.prevPath;
+        this.updateDom();
+        break;
+      default:
+        break;
+    }
   },
 });
--- node_helper.js
+++ node_helper.js
@@ -1,8 +1,68 @@
 const NodeHelper = require("node_helper");
+const Log = require("logger");
+let fs = require("fs");
+const multer = require("multer");
+const express = require("express");
+
+const upload = multer({
+  dest: "photos/",
+});
+
+const PAGE_SIZE = 20;
+
 module.exports = NodeHelper.create({
+  files: [],
+  photoIndex: 0,
+  interval: 10000,
   start: function () {
-    this.expressApp.get("/foobar", function (req, res) {
-      res.send("GET request to /foobar");
+    this.files = fs.readdirSync(`${this.path}/photos`);
+    this.expressApp
+      .route("/photos")
+      .post(upload.array("photos"), (req, res) => {
+        Log.log(req.files);
+        files.concat(req.files);
+      })
+      .delete(express.json(), (req, res) => {
+        Log.log(req.body);
+        res.json(req.body);
+      })
+      .get((req, res) => {
+        res.send(this.files);
+      });
+    this.expressApp.get("/photos/:photoId", (req, res) => {
+      const { photoId } = req.params;
+      res.sendFile(`${this.path}/photos/${photoId}`);
     });
+  },
+  getNextFiles: function () {
+    this.photoIndex += 1;
+    this.photoIndex =
+      this.photoIndex >= this.files.length ? 0 : this.photoIndex;
+    let prevIndex =
+      this.photoIndex - 1 < 0 ? this.files.length - 1 : this.photoIndex - 1;
+    Log.log(`${this.photoIndex} : ${prevIndex}`);
+    return {
+      nextFile: this.files[this.photoIndex] || null,
+      prevFile: this.files[prevIndex] || null,
+    };
+  },
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "GET_FIRST_IMAGES") {
+      this.sendSocketNotification("GET_FIRST_IMAGES", {
+        nextPath: this.files[this.photoIndex],
+        prevPath: null,
+      });
+      if (this.interval) {
+        clearInterval(this.interval);
+      }
+      this.interval = setInterval(() => {
+        const { nextFile, prevFile } = this.getNextFiles();
+        Log.log("update");
+        this.sendSocketNotification("GET_NEXT_IMAGE", {
+          nextPath: nextFile,
+          prevPath: prevFile,
+        });
+      }, this.interval);
+    }
   },
 });
