d65e3c616c25e5c21354a316addfcbc2d4ceb932
Noel Gomez <nollgmz4@gmail.com>
2024-09-29 10:06:08 -0700

    updated start

--- loki-frame/src/app/frame/components/ImageBackground/ConnectModal.tsx
+++ loki-frame/src/app/frame/components/ImageBackground/ConnectModal.tsx
@@ -1,20 +1,38 @@
 "use client";
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import { DotLottie, DotLottieReact } from "@lottiefiles/dotlottie-react";
+import { motion } from "framer-motion";
 import catLoad from "../cat_load.json";
+import { useEffect, useState } from "react";
 interface Props {
   addr: string;
 }
 export const ConnectModal = ({ addr }: Props) => {
+  const [lottieRef, setLottieRef] = useState<DotLottie | null>(null);
+  const [lottieLoaded, setLottieLoaded] = useState(false);
+  useEffect(() => {
+    const onLoad = () => setLottieLoaded(true);
+
+    lottieRef?.addEventListener("load", onLoad);
+    return () => {
+      lottieRef?.removeEventListener("load", onLoad);
+    };
+  }, [lottieRef]);
+
   return (
     <div className="size-full flex justify-center items-center">
-      <div className="bg-primary text-primary-content modal-box flex flex-col text-3xl justify-center items-center">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: lottieLoaded ? 1 : 0 }}
+        className="bg-primary text-primary-content modal-box flex flex-col text-3xl justify-center items-center"
+      >
         Connect to <b>{addr}</b>
         <DotLottieReact
           data={catLoad}
+          dotLottieRefCallback={setLottieRef}
           loop
           autoplay
         />
-      </div>
+      </motion.div>
     </div>
   );
 };
